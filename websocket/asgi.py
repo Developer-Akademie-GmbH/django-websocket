06d9a5ced880363ac02b919561b257e0609ddb76
Junus Ergin <junus91@googlemail.com>
2024-05-14 18:18:46 +0200

    04 - Websocket implementieren

--- websocket/asgi.py
+++ websocket/asgi.py
@@ -8,13 +8,21 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from chat.routing import websocket_urlpatterns
+
+
 django_asgi_app = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket.settings')
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+            ),
 })
