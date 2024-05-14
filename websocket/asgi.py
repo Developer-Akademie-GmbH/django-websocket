4bf6f6412c256b782e95e5acd53a15a7910583a6
Junus Ergin <junus91@googlemail.com>
2024-05-14 17:53:38 +0200

    first commit

---
+++ websocket/asgi.py
@@ -0,0 +1,19 @@
+"""
+ASGI config for websocket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket.settings')
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
